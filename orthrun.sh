6d53f90591ff8107b5d8f9efd6641bbb021b9049
Wessel Heerema <w.heerema@student.rug.nl>
2025-03-18 10:11:40 +0100

    had them set to the wrong values still, fixed

--- orthrun.sh
+++ orthrun.sh
@@ -1,3 +1,3 @@
 # Do an orthography run using 10.000 sentences from 1000 words, 4000 BPE merges and 5 epochs
 # Full agglutinative grammar
-python3 main.py sentences/gloss_10k.json sentences/agglutinative.json lexicon/lexicon-1k.json -m 10000 -e 5 -o results-ortho_10k.csv
+python3 main.py sentences/gloss_10k.json sentences/agglutinative.json lexicon/lexicon-1k.json -m 4000 -e 5 -o results-ortho_10k.csv
